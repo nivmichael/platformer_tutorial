4595d4ffd6627083b248d70ffd2c41fabe2d46da
AcadeMeData <michael@acade-me.com>
2020-08-10 09:38:23 +0300

    implement engine

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import pygame, sys, random
-
+import pygame, sys, os, random
+import data.engine as e
 clock = pygame.time.Clock()
 
 from pygame.locals import *
@@ -46,41 +46,7 @@ def generate_chunk(x, y):
     return chunk_data
 
 
-global animation_frames
-animation_frames = {}
-
-def load_animation(path, frame_duration):
-    global animation_frames
-    animation_name = path.split('/')[-1]
-    print(animation_name)
-    # loop to to show image shows in relevant frame (in a 60fps)
-    animation_frame_data = []
-    n = 0
-    for frame in frame_duration:
-        animation_frame_id = animation_name + '_' + str(n)
-        img_loc = path + '/' + animation_frame_id + '.png'
-        animation_image = pygame.image.load(img_loc).convert()
-        animation_image.set_colorkey((255, 255, 255))
-        # arrange the frames into global variable
-        animation_frames[animation_frame_id] = animation_image.copy()
-        for i in range(frame):
-            animation_frame_data.append(animation_frame_id)
-
-        n += 1
-    return animation_frame_data
-
-
-def change_action(action_var, frame, new_value):
-    if action_var != new_value:
-        action_var = new_value
-        frame = 0
-    return action_var, frame
-
-
-animation_database = {}
-
-animation_database['run'] = load_animation('player_animation/run', [7, 7])
-animation_database['idle'] = load_animation('player_animation/idle', [7, 7, 40])
+e.load_animations('data/images/entities/')
 
 player_action = 'idle'
 player_frame = 0
@@ -90,62 +56,29 @@ grass_sound_timer = 0
 
 game_map = {}
 
-grass_img = pygame.image.load('grass.png')
-dirt_img = pygame.image.load('dirt.png')
-plant_img = pygame.image.load('plant.png').convert()
+grass_img = pygame.image.load('data/images/grass.png')
+dirt_img = pygame.image.load('data/images/dirt.png')
+plant_img = pygame.image.load('data/images/plant.png').convert()
 plant_img.set_colorkey((255,255,255))
 
 tile_index = {1: grass_img,
               2: dirt_img,
               3: plant_img}
 
-jump_sound = pygame.mixer.Sound('jump.wav')
+jump_sound = pygame.mixer.Sound('data/audio/jump.wav')
 jump_sound.set_volume(0.2)
-grass_sound = [pygame.mixer.Sound('grass_0.wav'),  pygame.mixer.Sound('grass_1.wav')]
+grass_sound = [pygame.mixer.Sound('data/audio/grass_0.wav'),  pygame.mixer.Sound('data/audio/grass_1.wav')]
 grass_sound[0].set_volume(0.2)
 grass_sound[1].set_volume(0.2)
 
 
-pygame.mixer.music.load('music.wav')
+pygame.mixer.music.load('data/audio/music.wav')
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 
-
-player_rect = pygame.Rect(100, 100, 5, 13)
+player = e.entity(100, 100, 5, 13, 'player')
 
 background_objects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,40,40,400]],[0.5,[130,90,100,400]],[0.5,[300,80,120,400]]]
-
-
-def collision_test(rect, tiles):
-    hit_list = []
-    for tile in tiles:
-        if rect.colliderect(tile):
-            hit_list.append(tile)
-    return hit_list
-
-
-def move(rect, movement, tiles):
-    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
-    rect.x += movement[0]
-    hit_list = collision_test(rect, tiles)
-    for tile in hit_list:
-        if movement[0] > 0:
-            rect.right = tile.left
-            collision_types['right'] = True
-        elif movement[0] < 0:
-            rect.left = tile.right
-            collision_types['left'] = True
-    rect.y += int(movement[1])
-    hit_list = collision_test(rect, tiles)
-    for tile in hit_list:
-        if movement[1] > 0:
-            rect.bottom = tile.top
-            collision_types['bottom'] = True
-        elif movement[1] < 0:
-            rect.top = tile.bottom
-            collision_types['top'] = True
-    return rect, collision_types
-
 
 while True:  # game loop
     display.fill((146, 244, 255))  # clear screen by filling it with blue
@@ -153,8 +86,8 @@ while True:  # game loop
     if grass_sound_timer > 0:
         grass_sound_timer -= 1
     # lock the scroll to player location and center to the 300 display surface
-    true_scroll[0] += (player_rect.x - true_scroll[0] - 152) / 20
-    true_scroll[1] += (player_rect.y - true_scroll[1] - 106) / 20
+    true_scroll[0] += (player.x - true_scroll[0] - 152) / 20
+    true_scroll[1] += (player.y - true_scroll[1] - 106) / 20
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
@@ -197,18 +130,23 @@ while True:  # game loop
         vertical_momentum = 3
     # if moving on the X axis, we change the image to 'run'
     if player_movement[0] > 0:
-        player_action, player_frame = change_action(player_action, player_frame, 'run')
-        player_flip = False
+        player.set_action('run')
+        player.set_flip(False)
+        #player_action, player_frame = change_action(player_action, player_frame, 'run')
+        #player_flip = False
     if player_movement[0] == 0:
-        player_action, player_frame = change_action(player_action, player_frame, 'idle')
+        player.set_action('idle')
+        #player_action, player_frame = change_action(player_action, player_frame, 'idle')
     if player_movement[0] < 0:
-        player_action, player_frame = change_action(player_action, player_frame, 'run')
-        player_flip = True
+        player.set_action('run')
+        player.set_flip(True)
+        #player_action, player_frame = change_action(player_action, player_frame, 'run')
+        # player_flip = True
 
     # Move function with collisions
-    player_rect, collisions = move(player_rect, player_movement, tile_rects)
+    collisions_type = player.move(player_movement, tile_rects)
 
-    if collisions['bottom'] == True:
+    if collisions_type['bottom'] == True:
         air_timer = 0
         vertical_momentum = 0
         # check for grass sound
@@ -220,13 +158,11 @@ while True:  # game loop
     else:
         air_timer += 1
 
+
     # Get the correct image of player according to the action we're in, and the frame we're at
-    player_frame += 1
-    if player_frame >= len(animation_database[player_action]):
-        player_frame = 0
-    player_img_id = animation_database[player_action][player_frame]
-    player_img = animation_frames[player_img_id]
-    display.blit(pygame.transform.flip(player_img, player_flip, False), (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+    player.change_frame(1)
+    player.display(display, scroll)
+    #display.blit(pygame.transform.flip(player_img, player_flip, False), (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
     for event in pygame.event.get():  # event loop
         if event.type == QUIT:
